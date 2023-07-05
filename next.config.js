bb181d8492eebb8e8d6e0b81dd6f83a55e8356da
Vinay Selukar <vselukar2102@gmail.com>
2023-07-05 15:53:22 +0530

    made pwa test 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withOffline = require('next-offline')
 
-module.exports = nextConfig
+module.exports = withOffline({
+  reactStrictMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /[.](png|jpg|ico|css)/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'assets-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          }
+        }
+      },
+      {
+        urlPattern: /^http.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'http-cache',
+        }
+      }
+    ]
+  }
+})
