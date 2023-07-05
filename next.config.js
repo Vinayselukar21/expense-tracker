d1301268c8579574ea40cfceb948cd2ffba52fa3
Vinay Selukar <vselukar2102@gmail.com>
2023-07-05 17:33:19 +0530

    x

--- next.config.js
+++ next.config.js
@@ -1,28 +1,11 @@
-/** @type {import('next').NextConfig} */
-const withOffline = require('next-offline')
+const withPWA = require("next-pwa");
 
-module.exports = withOffline({
+module.exports = withPWA({
   reactStrictMode: true,
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /[.](png|jpg|ico|css)/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'assets-cache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          }
-        }
-      },
-      {
-        urlPattern: /^http.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'http-cache',
-        }
-      }
-    ]
-  }
-})
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
+export default MyApp;
